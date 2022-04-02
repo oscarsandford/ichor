22065d3f4581a9de62ef3ba634e538b6c90a66cc
Oscar Sandford <oscarsandford@gmail.com>
2022-04-02 10:36:39 -0700

    Add freehand drawing, colour keys, clear, etc.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "SDL.h"
 #include <iostream>
 #include <list>
+#include <string>
 
 #define WHITE {255, 255, 255, 255}
 #define OFF_WHITE {242, 242, 242, 255}
@@ -14,6 +15,15 @@ struct Color {
 };
 struct Line {
 	int x1,y1,x2,y2;
+};
+struct Point {
+	int x,y;
+};
+
+template <typename T>
+struct Obj {
+	T feature;
+	std::string type;
 	Color color;
 };
 
@@ -41,16 +51,16 @@ public:
 		SDL_Quit();
 	}
 	
-	void draw(int x, int y, int x2, int y2)
+	void draw_line(int x, int y, int x2, int y2, Color color)
 	{
-		SDL_SetRenderDrawColor(renderer, pen_color.r, pen_color.g, pen_color.b, pen_color.a);
+		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
 		SDL_RenderDrawLine(renderer, x, y, x2, y2);
 	}
 
-	void pen_toggle()
+	void draw_point(int x, int y, Color color)
 	{
-		pen_color = BLUE;
-		std::cout << "Changed colour" << std::endl;
+		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+		SDL_RenderDrawPoint(renderer, x, y);
 	}
 
 	void clear()
@@ -59,11 +69,15 @@ public:
 	        SDL_RenderClear(renderer);
 	}
 
-	void update(std::list<Line> &li)
+	void update(std::list<Obj<Point>> &pli, std::list<Obj<Line>> &lli)
 	{
-		for (auto const &l : li) {
-			SDL_SetRenderDrawColor(renderer, l.color.r, l.color.g, l.color.b, l.color.a);
-			SDL_RenderDrawLine(renderer, l.x1, l.y1, l.x2, l.y2);
+		for (auto const &obj : pli) {
+			SDL_SetRenderDrawColor(renderer, obj.color.r, obj.color.g, obj.color.b, obj.color.a);
+			SDL_RenderDrawPoint(renderer, obj.feature.x, obj.feature.y);		
+		}
+		for (auto const &obj : lli) {
+			SDL_SetRenderDrawColor(renderer, obj.color.r, obj.color.g, obj.color.b, obj.color.a);
+			SDL_RenderDrawLine(renderer, obj.feature.x1, obj.feature.y1, obj.feature.x2, obj.feature.y2);
 		}
 	}
 
@@ -71,7 +85,6 @@ public:
 	{
 		SDL_RenderPresent(renderer);
 	}
-	Color pen_color = RED;
 
 private:
 	int height, width;
@@ -84,12 +97,23 @@ int main(int argc, char *argv[])
 {
 	IchWin ich(640, 480);
 	
-	std::list<Line> drawn_lines;
+	/* 
+		Having two lists here is not super aesthetic, and kind of 
+		defeats the purpose of what I was going for with a 
+		generic drawn object type. It works, but it could be 
+		neater and more extensible. Work on this later.
+	*/
+	std::list<Obj<Point>> drawn_points;
+	std::list<Obj<Line>> drawn_lines;
 
-	SDL_Event event;
 	bool mouse_pressed = false;
+	// False: line, True: freehand.	
+	bool mode = true;
+	Color pen_color = BLACK;
 	int x1,x2,y1,y2;
 	
+	SDL_Event event;
+
 	while (1) {
 		SDL_Delay(10);
 
@@ -109,26 +133,63 @@ int main(int argc, char *argv[])
 			}
 			// Unclick event.
 			else if (event.type == SDL_MOUSEBUTTONUP) {
-				Line l = {x1,y1,x2,y2,ich.pen_color};
-				drawn_lines.push_back(l);
+				if (mode) { // Freehand
+					Point feature = {x1,y1};
+					Obj<Point> obj = {feature, "point", pen_color};
+					drawn_points.push_back(obj);
+				}
+				else {				
+					Line feature = {x1,y1,x2,y2};
+					Obj<Line> obj = {feature, "line", pen_color};
+					drawn_lines.push_back(obj);
+				}
 				mouse_pressed = false;
 			}
 			// Draw event (drag with mouse down).
 			else if (event.type == SDL_MOUSEMOTION && mouse_pressed) {
 				x2 = event.motion.x;
 				y2 = event.motion.y;
+				if (mode) { // Freehand: draw points on drag.
+					Point feature = {x2,y2};
+					Obj<Point> obj = {feature, "point", pen_color};
+					drawn_points.push_back(obj);
+				}
 			}
 			// Switch colour.
 			else if (event.type == SDL_KEYDOWN) {
-				ich.pen_toggle();
+				switch (event.key.keysym.sym) {
+					case SDLK_1: // Color swaps.
+						pen_color = RED;
+						break;
+					case SDLK_2:
+						pen_color = GREEN;
+						break;
+					case SDLK_3:
+						pen_color = BLUE;
+						break;
+					case SDLK_4:
+						pen_color = BLACK;
+						break;
+					case SDLK_SPACE: // Switch mode (freehand/line)
+						mode = !mode;
+						break;
+					case SDLK_ESCAPE:
+						drawn_points.clear();
+						drawn_lines.clear();
+						break;
+				}				
 			}
 		}
 
 		ich.clear();
 		if (mouse_pressed) {
-			 ich.draw(x1, y1, x2, y2);
+			if (mode) // Freehand
+				ich.draw_point(x1, y1, pen_color);
+			else // Line 
+				ich.draw_line(x1, y1, x2, y2, pen_color);
 		}
-		ich.update(drawn_lines);
+
+		ich.update(drawn_points, drawn_lines);
 		ich.render();
 	}		
 	return 0;
